d36bd64ec562b7f579d62f3430ee6d072cf627dc
Kevin Hookumchand <khookumchand@gmail.com>
2021-11-21 12:25:45 -0500

    Added RSpec validations to model

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:ingredients) }
+  it { should validate_presence_of(:instructionsy) }
 end
